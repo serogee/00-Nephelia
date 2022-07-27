af4b8f9de77b4a73f7ac025824032e090c5a39ad
serosero <102843209+serogee@users.noreply.github.com>
2022-07-27 19:41:10 +0800

    Added db handler and commander file

---
+++ db.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+connections = dict()
+
+def connect(database, *args, **kwargs):
+    if database in connection:
+        return connection[database]
+    connections[database] = conn = sqlite3.connect(database=database, *args, **kwargs)
+    return conn
